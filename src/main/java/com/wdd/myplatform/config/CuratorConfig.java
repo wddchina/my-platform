b33b364a7708d16500aaa504d7eb6aa8ad1a0a7f
wangdongdong <wangdongdong@haier.com>
2021-04-09 17:54:09 +0800

    整合zookeeper

---
+++ src/main/java/com/wdd/myplatform/config/CuratorConfig.java
@@ -0,0 +1,23 @@
+package com.wdd.myplatform.config;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CuratorConfig {
+
+    @Value("${zookeeper.address}")
+    private String address;
+
+    @Bean(initMethod="start",destroyMethod = "close")
+    public CuratorFramework getCuratorFramework() {
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        CuratorFramework client = CuratorFrameworkFactory.newClient(address, retryPolicy);
+        return client;
+    }
+}
